69a27ccbcc9f7e4d70ff808ea8032be38e05db45
midori.shinozaki <bltrefutation@gmail.com>
2023-08-23 22:42:56 +0000

    follow,unfollow

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -5,14 +5,14 @@
         <aside class="mt-4">
             {{-- ユーザ情報 --}}
             @include('users.card')
-        </aside>    
-         <div class="sm:col-span-2 mt-4">
-            {{-- タブ --}}  
+        </aside>
+        <div class="sm:col-span-2 mt-4">
+            {{-- タブ --}}
             @include('users.navtabs')
-            {{-- 投稿フォーム --}}
-            @include('microposts.form')
-            {{-- 投稿一覧 --}}
-            @include('microposts.microposts')
+            <div class="mt-4">
+                {{-- ユーザ一覧 --}}
+                @include('users.users')
+            </div>
         </div>
     </div>
 @endsection
